e42896082401ce655e5c5ffe016105bc77a20713
joeltankam <17796723+joeltankam@users.noreply.github.com>
2025-11-03 20:15:40 +0100

    Update home page animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -118,11 +118,36 @@ onMounted(() => {
         }
       },
       interactivity: {
-        detect_on: 'canvas',
+        detect_on: 'window',
         events: {
           onhover: { enable: true, mode: 'repulse' },
           onclick: { enable: true, mode: 'push' },
           resize: true
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 100,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
         }
       },
       retina_detect: true
