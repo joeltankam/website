147cfadf1f4a53cd043dcc77b976792fcc4cf34d
joeltankam <17796723+joeltankam@users.noreply.github.com>
2025-11-13 13:17:39 +0100

    Remove email

--- src/views/About.vue
+++ src/views/About.vue
@@ -206,13 +206,6 @@
             </svg>
             Bluesky
           </a>
-
-          <a href="mailto:joel.tankam@gmail.com" class="inline-flex items-center px-6 py-3 bg-secondary-600 text-white rounded-xl hover:bg-secondary-700 transition-colors">
-            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-            </svg>
-            Email
-          </a>
         </div>
 
         <div class="mt-8 pt-8 border-t border-gray-200">
